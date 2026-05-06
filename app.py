2af6bc79bdc2dfdf76b52c9b0a5cac038684dacd
adameehan8921-debug <adameehan8921@gmail.com>
2026-05-06 16:57:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from peft import PeftModel
 # 🔐 Encoded model names
 # =========================
 base_model_enc = "ZmFjZWJvb2svb3B0LTEyNW0="
-adapter_enc = "QWlyYWFkYW0vYWlyYS1haQ=="
+adapter_enc = "QWRhbWhlcmUvYWlyYS1haQ=="
 
 # Decode
 base_model = base64.b64decode(base_model_enc).decode()
